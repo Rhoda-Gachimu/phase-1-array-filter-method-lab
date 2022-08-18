16d4f1d612a2c02e69f4cdb3b242b0a2897f489d
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-08-19 02:07:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, str) {
+    return array.filter(name => name.toLowerCase() === str.toLowerCase())
+}
+
+function fuzzyMatch(array, str) {
+    return array.filter(name => name.startsWith(str))
+}
+
+function matchName(array, str) {
+    return array.filter(obj => obj.name === str)
+}
